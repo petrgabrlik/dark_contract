d4f9b7651ac730121d5e4427a6d5743b11f0139c
Petr Gábrlík <petr.gabrlik@ceitec.vutbr.cz>
2017-10-05 13:13:20 +0200

    file numbering added

--- get_data.py
+++ get_data.py
@@ -6,6 +6,7 @@ Get data from a factory in Kazachstan.
 
 import requests
 import simplejson as json
+import os
 
 IP = '165.227.157.145'
 PORT = '8080'
@@ -15,13 +16,19 @@ def main():
     '''
     Main function.
     '''
+
+    i = 0
+    while os.path.exists("data{}.txt".format(i)):
+        i += 1
+
     for x in range(-10, 11, 1):
         y = list()
         for rep in range(10):
             r = requests.get('http://'+IP+':'+PORT+DIR, params = {'x': x}).text
             # print(r)
             y.append(json.loads(r)['data']['y'])
-        with open('data.txt', 'a') as f:
+
+        with open('data{}.txt'.format(i), 'a') as f:
             print(str(x) + '\t' + '\t'.join(map(str,y)), file = f)
 
     print('ok')
