f8f53b07cbda51fffc5db7492714cbbc6ab69870
Petr Gábrlík <petr.gabrlik@ceitec.vutbr.cz>
2017-10-13 17:16:09 +0200

    argument passing and parsing implemented

--- get_data.py
+++ get_data.py
@@ -1,5 +1,5 @@
 '''
-Get data from a factory in Kazachstan.
+Get data from the factory in Kazachstan.
 
 Entry task: https://github.com/kiwicom/mlweekend
 Address example: http://165.227.157.145:8080/api/do_measurement?x=2
@@ -9,33 +9,31 @@ import requests
 import simplejson as json
 import os
 import numpy as np
+import argparse
 
 IP = '165.227.157.145'
 PORT = '8080'
 DIR = '/api/do_measurement'
 
-def main():
+def main(limit_l, limit_h, step, rep):
     '''
     Main function.
     '''
 
+    # Generate filename
     i = 0
     while os.path.exists("data{}.txt".format(i)):
         i += 1
     filename = "data{}.txt".format(i)
 
-    limit_l = -0.5
-    limit_h = 0.5
-    step = 0.1
-    repetition = 10
     steps = (limit_h - limit_l) / step
     c = 0
 
-    print('Interval {},{}, step {}, repetition {}'.format(limit_l, limit_h, step, repetition))
+    print('Interval {},{}, step {}, rep {}'.format(limit_l, limit_h, step, rep))
 
     for x in np.arange(limit_l, limit_h, step):
         y = list()
-        for rep in range(repetition):
+        for i in range(rep):
             r = requests.get('http://'+IP+':'+PORT+DIR, params = {'x': x}).text
             # print(r)
             y.append(json.loads(r)['data']['y'])
@@ -52,4 +50,14 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # Instantiate the parser
+    parser = argparse.ArgumentParser(description='App downloads data from the factory in Kazachstan.')
+    # Required positional argument
+    parser.add_argument('limit_l', type=float, help='Lower limit, a required float positional argument')
+    parser.add_argument('limit_h', type=float, help='Upper limit, a required float positional argument')
+    parser.add_argument('step', type=float, help='Iteration step, a required float positional argument')
+    parser.add_argument('rep', type=int, help='The number of repetions, a required integer positional argument')
+    # Parse
+    args = parser.parse_args()
+
+    main(args.limit_l, args.limit_h, args.step, args.rep)
