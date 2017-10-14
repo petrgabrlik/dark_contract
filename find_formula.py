d7b65ca875472b1bc392c5e435f1b81fdeadcd54
Petr Gabrlik <petrgabrlik@email.cz>
2017-10-14 22:01:06 +0200

    app now compare input data with polynomial function and y=x^4 function

--- find_formula.py
+++ find_formula.py
@@ -5,9 +5,21 @@ Entry task: https://github.com/kiwicom/mlweekend
 Address example: http://165.227.157.145:8080/api/do_measurement?x=2
 '''
 
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
-import argparse
+
+
+def filter_data(data):
+    '''
+
+    '''
+    # Copy x values and average y values
+    num_of_rows = np.shape(data)[0]
+    data_filt = np.zeros([num_of_rows,2])
+    data_filt[:,0] = data[:,0]
+    data_filt[:,1] = np.apply_along_axis( np.average, axis=1, arr=data[:,1:])
+    return data_filt
 
 
 def main(filename):
@@ -18,38 +30,56 @@ def main(filename):
     # Load data file
     data = np.loadtxt(filename)
 
-    # Copy x values and averaged y values
-    num_of_rows = np.shape(data)[0]
-    data_filt = np.zeros([num_of_rows,2])
-    data_filt[:,0] = data[:,0]
-    data_filt[:,1] = np.apply_along_axis( np.average, axis=1, arr=data[:,1:])
+    # Filter data
+    data_filt = filter_data(data)
 
-    # print(data_filt)
+    # Plot input data
+    plt.figure(1)
+    plt.plot(data_filt[:,0], data_filt[:,1],'-+r')
+    plt.xlabel('x', fontweight='bold')
+    plt.ylabel('y', fontweight='bold')
+    plt.title('Filtered input data from {} file'.format(filename), fontweight='bold')
+    plt.show()
 
-    # poly = np.polyfit(data_filt[:,0], data_filt[:,1], 4)
-    [poly, residuals, rank, singular_values, rcond] = np.polyfit(data_filt[:,0], data_filt[:,1], 4, full=True)
-    print(residuals)
-    p = np.poly1d(poly)
-
-    print(poly)
-    print(p)
-
-    # Function y=x^4 for comparision
+    # Compare data with function y=x^4
     p_x4 = np.poly1d([1, 0, 0, 0, 0])
-
     # Generate dense x array for plotting purposes
     x_dense = np.arange(np.ndarray.min(data[:,0]),
         np.ndarray.max(data[:,0]) + (np.ndarray.max(data[:,0]) - np.ndarray.min(data[:,0])) / 100,
         (np.ndarray.max(data[:,0]) - np.ndarray.min(data[:,0])) / 100)
-
-    # Plot data
+    plt.figure(2)
     plt.plot(data_filt[:,0], data_filt[:,1],'+r',
-        x_dense, p(x_dense),'--b',
         x_dense, p_x4(x_dense), '--g')
-    plt.xlabel(r'$\bf{x}$')
-    plt.ylabel(r'$\bf{y}$')
-    # plt.title(p)
-    # plt.grid()
+    plt.xlabel('x', fontweight='bold')
+    plt.ylabel('y', fontweight='bold')
+    plt.title('Input data compared with y=x^4 function', fontweight='bold')
+    plt.legend(['Input data', 'y=x^4'])
+    plt.show()
+
+    # Find best polynomial fit
+    o = 4 # Polynomial order
+    poly = np.polyfit(data_filt[:,0], data_filt[:,1], o)
+    p = np.poly1d(poly)
+    print(p)
+    plt.figure(3)
+    plt.plot(data_filt[:,0], data_filt[:,1],'+r',
+        x_dense, p(x_dense),'--b')
+        # x_dense, p_x4(x_dense), '--g')
+    plt.xlabel('x', fontweight='bold')
+    plt.ylabel('y', fontweight='bold')
+    plt.title('Polynomial order {}'.format(o), fontweight='bold')
+    plt.legend(['Input data', 'Polynomial function'])
+    plt.show()
+
+    # Plot all together
+    plt.figure(4)
+    plt.plot(data_filt[:,0], data_filt[:,1],'+r',
+        x_dense, p_x4(x_dense), '--g',
+        x_dense, p(x_dense),'--b')
+    plt.xlabel('x', fontweight='bold')
+    plt.ylabel('y', fontweight='bold')
+    plt.title('Polynomial order {}'.format(o), fontweight='bold')
+    plt.legend(['Input data', 'y=x^4 function', 'Polynomial function'])
     plt.show()
 
 
