f5fb00858d7a4dec18c18518af93ef424677bc70
Petr Gabrlik <petrgabrlik@email.cz>
2017-10-17 20:53:58 +0200

    comments, shebang line

--- get_data.py
+++ get_data.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 '''
 Get data from the factory in Kazachstan.
 
@@ -11,13 +12,15 @@ import os
 import numpy as np
 import argparse
 
+
 IP = '165.227.157.145'
 PORT = '8080'
 DIR = '/api/do_measurement'
 
+
 def main(limit_l, limit_h, step, rep):
     '''
-    Main function.
+    Everything happens here.
     '''
 
     # Generate filename
@@ -29,7 +32,7 @@ def main(limit_l, limit_h, step, rep):
     steps = (limit_h - limit_l) / step
     c = 0
 
-    print('Interval {},{}, step {}, rep {}'.format(limit_l, limit_h, step, rep))
+    print('Interval {},{}, step {}, repetitions {}'.format(limit_l, limit_h, step, rep))
 
     for x in np.arange(limit_l, limit_h, step):
         y = list()
@@ -53,10 +56,10 @@ if __name__ == '__main__':
     # Instantiate the parser
     parser = argparse.ArgumentParser(description='App downloads data from the factory in Kazachstan.')
     # Required positional argument
-    parser.add_argument('limit_l', type=float, help='Lower limit, a required float argument')
-    parser.add_argument('limit_h', type=float, help='Upper limit, a required float argument')
+    parser.add_argument('limit_l', type=float, help='Lower interval limit, a required float argument')
+    parser.add_argument('limit_h', type=float, help='Upper interval limit, a required float argument')
     parser.add_argument('step', type=float, help='Iteration step, a required float argument')
-    parser.add_argument('rep', type=int, help='The number of repetions, a required integer argument')
+    parser.add_argument('rep', type=int, help='The number of repetitions, a required integer argument')
     # Parse
     args = parser.parse_args()
 
