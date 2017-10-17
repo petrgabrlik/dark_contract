1e5116a4710d6e9235dd6f6c9a4884b29f6da73f
Petr Gabrlik <petrgabrlik@email.cz>
2017-10-17 19:13:41 +0200

    figure saving and several minor changes

--- find_formula.py
+++ find_formula.py
@@ -9,6 +9,9 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
+ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4])
+
+
 def count_missing(data):
     '''
     Function returns the count of missing values.
@@ -33,7 +36,7 @@ def interpolate_missing(data):
 
 def filter_data(data):
     '''
-
+    Average multiple measured data.
     '''
     # Copy x values and average y values
     num_of_rows = np.shape(data)[0]
@@ -45,7 +48,7 @@ def filter_data(data):
 
 def main(filename):
     '''
-
+    The workflow.
     '''
     # Load data file
     # data = np.loadtxt(filename)
@@ -68,54 +71,60 @@ def main(filename):
     data_filt = filter_data(data)
     print(data_filt,'\n')
 
-    # # Plot input data
-    # plt.figure(1)
-    # plt.plot(data_filt[:,0], data_filt[:,1],'-+r')
-    # plt.xlabel('x', fontweight='bold')
-    # plt.ylabel('y', fontweight='bold')
-    # plt.title('Filtered input data from {} file'.format(filename), fontweight='bold')
+    # Plot input data
+    plt.figure(1)
+    plt.plot(data_filt[:,0], data_filt[:,1], '+b', markersize=10)
+    plt.xlabel('x', fontweight='bold')
+    plt.ylabel('y', fontweight='bold')
+    plt.title('Filtered input data from {} file'.format(filename), fontweight='bold')
     # plt.show()
-    #
-    # # Compare data with function y=x^4
-    # p_x4 = np.poly1d([1, 0, 0, 0, 0])
-    # # Generate dense x array for plotting purposes
-    # x_dense = np.arange(np.ndarray.min(data[:,0]),
-    #     np.ndarray.max(data[:,0]) + (np.ndarray.max(data[:,0]) - np.ndarray.min(data[:,0])) / 100,
-    #     (np.ndarray.max(data[:,0]) - np.ndarray.min(data[:,0])) / 100)
-    # plt.figure(2)
-    # plt.plot(data_filt[:,0], data_filt[:,1],'+r',
-    #     x_dense, p_x4(x_dense), '--g')
-    # plt.xlabel('x', fontweight='bold')
-    # plt.ylabel('y', fontweight='bold')
-    # plt.title('Input data compared with y=x^4 function', fontweight='bold')
-    # plt.legend(['Input data', 'y=x^4'])
+    plt.savefig('data_input.png', bbox_inches='tight')
+
+    # Compare data with function y=x^4
+    p_x4 = np.poly1d([1, 0, 0, 0, 0])
+    # Generate dense x array for plotting purposes
+    x_dense = np.arange(np.ndarray.min(data[:,0]),
+        np.ndarray.max(data[:,0]) + (np.ndarray.max(data[:,0]) - np.ndarray.min(data[:,0])) / 100,
+        (np.ndarray.max(data[:,0]) - np.ndarray.min(data[:,0])) / 100)
+    plt.figure(2)
+    plt.plot(data_filt[:,0], data_filt[:,1], '+b',
+        x_dense, p_x4(x_dense), '--g', markersize=10)
+    plt.xlabel('x', fontweight='bold')
+    plt.ylabel('y', fontweight='bold')
+    plt.title('y=x^4 approximation', fontweight='bold')
+    plt.legend(['Input data', 'y=x^4'])
     # plt.show()
-    #
-    # # Find best polynomial fit
-    # o = 4 # Polynomial order
-    # poly = np.polyfit(data_filt[:,0], data_filt[:,1], o)
-    # p = np.poly1d(poly)
-    # print(p)
-    # plt.figure(3)
-    # plt.plot(data_filt[:,0], data_filt[:,1],'+r',
-    #     x_dense, p(x_dense),'--b')
-    #     # x_dense, p_x4(x_dense), '--g')
-    # plt.xlabel('x', fontweight='bold')
-    # plt.ylabel('y', fontweight='bold')
-    # plt.title('Polynomial order {}'.format(o), fontweight='bold')
-    # plt.legend(['Input data', 'Polynomial function'])
+    plt.savefig('data_input_x4.png', bbox_inches='tight')
+
+    # Find best polynomial fit
+    o = 4 # Polynomial order
+    poly = np.polyfit(data_filt[:,0], data_filt[:,1], o)
+    p = np.poly1d(poly)
+    print(p)
+    plt.figure(3)
+    plt.plot(data_filt[:,0], data_filt[:,1], '+b',
+        x_dense, p(x_dense), ':r', markersize=10)
+        # x_dense, p_x4(x_dense), '--g')
+    plt.xlabel('x', fontweight='bold')
+    plt.ylabel('y', fontweight='bold')
+    plt.title('{} order polynomial approximation'.format(ordinal(o)), fontweight='bold')
+    plt.legend(['Input data', '{} order polynomial'.format(ordinal(o))])
     # plt.show()
-    #
-    # # Plot all together
-    # plt.figure(4)
-    # plt.plot(data_filt[:,0], data_filt[:,1],'+r',
-    #     x_dense, p_x4(x_dense), '--g',
-    #     x_dense, p(x_dense),'--b')
-    # plt.xlabel('x', fontweight='bold')
-    # plt.ylabel('y', fontweight='bold')
-    # plt.title('Polynomial order {}'.format(o), fontweight='bold')
-    # plt.legend(['Input data', 'y=x^4 function', 'Polynomial function'])
+    plt.savefig('data_input_p4.png', bbox_inches='tight')
+
+    # Plot all together
+    plt.figure(4)
+    plt.plot(data_filt[:,0], data_filt[:,1],'+b',
+        x_dense, p_x4(x_dense), '--g',
+        x_dense, p(x_dense), ':r', markersize=10)
+    plt.xlabel('x', fontweight='bold')
+    plt.ylabel('y', fontweight='bold')
+    plt.title('Comparison of approximations', fontweight='bold')
+    plt.legend(['Input data', 'y=x^4', '{} order polynomial'.format(ordinal(o))])
     # plt.show()
+    plt.savefig('data_input_x4_p4.png', bbox_inches='tight')
+
+    plt.show()
 
 
 if __name__ == '__main__':
